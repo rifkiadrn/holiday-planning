5977c98933d4a44964356d788f671b3c24ee6116
rifkiadrn <rifkiadrn12@gmail.com>
2019-05-01 16:31:46 +0700

    add visit print

--- itinerary.pl
+++ itinerary.pl
@@ -2,6 +2,9 @@
 * first param is the spot name(unique), second param is the opening time,
 * third param is the closing time, and fourth param is the category
 */
+
+:- dynamic totalVisit/1.
+
 spot(a, 6, 10, culinary).
 spot(b, 7, 11, culinary).
 spot(c, 10, 11, culinary).
@@ -13,15 +16,34 @@ spot(f, 14, 15, culinary).
 * first param is the source node, second param is the destination node (vice versa/undirected graph),
 * third param is the weight \/ edge of the graph representing the time needed to journey to the end node
 */
+
+edge(home, a, 1).
+edge(home, c, 3).
+edge(home, e, 5).
 edge(a, b, 1).
 edge(b, c, 2).
 edge(c, d, 1).
-edge(b, d, 4).
+edge(b, d, 1).
 edge(c, e, 1).
-edge(e, f, 6).
+edge(e, f, 2).
 
-path(X, Y, Res, Input, [X|Ys]):- spot(X, _, _, Input), edge(X, Z, Weight), spot(Z, _, _, Input), path(Z, Y, Weight2, Input, Ys), Res is Weight + Weight2.
-path(X, X, 0, Input, [X]):- spot(X, _, _, Input).
+
+/**
+* print visited spot
+*/
+visit(Destination,Weight, CurrentTime):- write(CurrentTime), write(' - '), UpdatedWithJourneyTime is CurrentTime + Weight, write(UpdatedWithJourneyTime), write(': Journey to Vacation Spot '), write(Destination), nl, UpdatedWithVisitTime is UpdatedWithJourneyTime + 1, write(UpdatedWithJourneyTime), write(' - '), write(UpdatedWithVisitTime), write(': Vacation Spot '), write(Destination), nl. 
+
+/**
+* update total visit in db
+*/
+incrementTotalVisit:- \+ totalVisit(_), !, assert(totalVisit(1)).
+incrementTotalVisit:- totalVisit(X), !, NewX is X + 1, retract(totalVisit(_)), assert(totalVisit(NewX)).
+
+/**
+* traverse the graph
+*/
+path(X, Y, Input, [Z|Ys], CurrentTime):- edge(X, Z, Weight), spot(Z, _, _, Input), visit(Z, Weight, CurrentTime), UpdatedTime is CurrentTime + 1 + Weight, incrementTotalVisit, path(Z, Y, Input, Ys, UpdatedTime).
+path(X, _, Input, [], _):- spot(X, _, _, Input).
 
 %starting the program with predicate go/0
-% go :- read(Input),path(X, Y, Res, Input), write(X), nl, write(Y), nl, write(Res).
+go :- read(Input), retractall(totalVisit(_)), path(home, _, Input, SPOT, 8), write(SPOT), write('Total: '), totalVisit(Total), write(Total).
