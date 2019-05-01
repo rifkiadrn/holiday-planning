6d2a53f82383a507f38880839cc8129d9d74573f
reinhardhotma <reinhard_hotma@yahoo.com>
2019-05-02 01:04:16 +0700

    add time spent to spot

--- itinerary.pl
+++ itinerary.pl
@@ -1,16 +1,17 @@
 /** spot/4 is the predicate for a spot object
 * first param is the spot name(unique), second param is the opening time,
-* third param is the closing time, and fourth param is the category
+* third param is the closing time, and fourth param is the time spent,
+* and the fifth param is the category
 */
 
 :- dynamic totalVisit/1.
 
-spot(a, 6, 10, culinary).
-spot(b, 7, 11, culinary).
-spot(c, 10, 11, culinary).
-spot(d, 12, 11, culinary).
-spot(e, 13, 18, culinary).
-spot(f, 14, 15, culinary).
+spot(a, 6, 10, 2, culinary).
+spot(b, 7, 11, 1, culinary).
+spot(c, 10, 11, 1, culinary).
+spot(d, 12, 11, 3, culinary).
+spot(e, 13, 18, 2, culinary).
+spot(f, 14, 15, 1, culinary).
 
 /** edge/3 is the predicate to simulate the weighted graph
 * first param is the source node, second param is the destination node (vice versa/undirected graph),
@@ -31,7 +32,10 @@ edge(e, f, 2).
 /**
 * print visited spot
 */
-visit(Destination,Weight, CurrentTime):- write(CurrentTime), write(' - '), UpdatedWithJourneyTime is CurrentTime + Weight, write(UpdatedWithJourneyTime), write(': Journey to Vacation Spot '), write(Destination), nl, UpdatedWithVisitTime is UpdatedWithJourneyTime + 1, write(UpdatedWithJourneyTime), write(' - '), write(UpdatedWithVisitTime), write(': Vacation Spot '), write(Destination), nl. 
+visit(Destination, Weight, TimeSpent, CurrentTime):- write(CurrentTime), write(' - '), UpdatedWithJourneyTime is 
+ CurrentTime + Weight, write(UpdatedWithJourneyTime), write(': Journey to Vacation Spot '), write(Destination), nl, UpdatedWithVisitTime is 
+ UpdatedWithJourneyTime + TimeSpent, write(UpdatedWithJourneyTime), write(' - '), write(UpdatedWithVisitTime), 
+ write(': Vacation Spot '), write(Destination), nl.
 
 /**
 * update total visit in db
@@ -42,8 +46,9 @@ incrementTotalVisit:- totalVisit(X), !, NewX is X + 1, retract(totalVisit(_)), a
 /**
 * traverse the graph
 */
-path(X, Y, Input, [Z|Ys], CurrentTime):- edge(X, Z, Weight), spot(Z, _, _, Input), visit(Z, Weight, CurrentTime), UpdatedTime is CurrentTime + 1 + Weight, incrementTotalVisit, path(Z, Y, Input, Ys, UpdatedTime).
-path(X, _, Input, [], _):- spot(X, _, _, Input).
+path(X, Y, Category, [Z|Ys], CurrentTime):- edge(X, Z, Weight), spot(Z, _, _, TimeSpent, Category), visit(Z, Weight, TimeSpent, CurrentTime), 
+ UpdatedTime is CurrentTime + TimeSpent + Weight, incrementTotalVisit, path(Z, Y, Category, Ys, UpdatedTime).
+path(X, _, Category, [], _):- spot(X, _, _, _, Category).
 
 %starting the program with predicate go/0
-go :- read(Input), retractall(totalVisit(_)), path(home, _, Input, SPOT, 8), write(SPOT), write('Total: '), totalVisit(Total), write(Total).
+go :- read(Category), retractall(totalVisit(_)), path(home, _, Category, SPOT, 8), write(SPOT), nl, write('Total: '), totalVisit(Total), write(Total).
